70399c585ec2f571bfe17a797f55efa4148b49ec
Julian McClellan <juliancmcclellan@gmail.com>
2019-11-01 14:05:44 -0500

    Probably want to use node JS 'hltv' package to properly parse events then download demos (single thread because HLTV are good people)

--- reddit/scrape.py
+++ reddit/scrape.py
@@ -2,12 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import praw
-
-PRAW_AGENT = praw.Reddit("csgo",  # Site ID
+POST_MATCH_TEXT = "Discussion | Esports"
+LINK_FLAIR_TEMPLATE_ID = "af5b57fc-374a-11e6-abd9-0e7c62f92521"
+PRAW_AGENT = praw.Reddit("csgo",
                          user_agent="csgo user agent"
                          )
 
 print(PRAW_AGENT.read_only)
 
-for submission in PRAW_AGENT.subreddit("GlobalOffensive").hot(limit=10):
-    print(submission.flair)
+subreddit = PRAW_AGENT.subreddit("GlobalOffensive")
+test_submission = PRAW_AGENT.submission(url="https://old.reddit.com/r/GlobalOffensive/comments/cwj33v/ence_vs_avangar_starladder_major_berlin_2019_the/")
+
+match_summaries = subreddit.search('flair:"Discussion | Esports" self:yes', limit=1000)
+count = 0
+for subi in match_summaries:
+    count += 1
+    title = subi.title
+    print(count, title, subi.author)
